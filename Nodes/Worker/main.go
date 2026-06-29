c792b855f3527c1fa348bbd5db2fc22f218dd0ed
Tymon Tumialis <tymontumialis@gmail.com>
2026-06-29 10:39:36 +0200

    automative heartbeat

--- Nodes/Worker/main.go
+++ Nodes/Worker/main.go
@@ -11,6 +11,34 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+func SendHeartbeatToMaster(client pb.HealthServiceClient) {
+	req := &pb.HeartbeatRequest{
+		WorkerId:  "worker-node-01",
+		Timestamp: time.Now().Unix(),
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	res, err := client.SendHeartbeat(ctx, req)
+	if err != nil {
+		log.Fatalf("Error sending heartbeat : %v", err)
+	}
+	log.Printf("Response from master %v", res.GetAcknowledge())
+}
+
+func startHeartbeatLoop(conn *grpc.ClientConn, interval time.Duration) {
+	client := pb.NewHealthServiceClient(conn)
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			SendHeartbeatToMaster(client)
+		}
+	}
+
+}
+
 func main() {
 
 	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -19,19 +47,7 @@ func main() {
 	}
 	defer conn.Close()
 
-	client := pb.NewHealthServiceClient(conn)
-
-	req := &pb.HeartbeatRequest{
-		WorkerId:  "worker-node-01",
-		Timestamp: time.Now().Unix(),
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	res, err := client.SendHeartbeat(ctx, req)
-	if err != nil {
-		log.Fatalf("Error sending heartbeat : %v", err)
-	}
-	log.Printf("Response from master %v", res.GetAcknowledge())
+	go startHeartbeatLoop(conn, 3*time.Second)
+	log.Println("Worker started")
+	select {}
 }
